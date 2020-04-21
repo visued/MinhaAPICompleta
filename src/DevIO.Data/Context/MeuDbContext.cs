7f71fe18dad11aee834707cc3dd72046d589f28b
Victor Sued <visued@gmail.com>
2020-04-21 13:25:23 -0300

    Projeto com CSHARP ASPNET CORE + ORACLE

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseOracle("DATA SOURCE=172.17.0.2:1521/oracle_12g;PERSIST SECURITY INFO=True;USER ID=sys; password=123456; Pooling =False;")
+        //     .UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
+        // }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+                property.Relational().ColumnType = "varchar(100)";
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+
+    public class ApplicationContextDbFactory : IDesignTimeDbContextFactory<MeuDbContext>
+    {
+        MeuDbContext IDesignTimeDbContextFactory<MeuDbContext>.CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MeuDbContext>();
+            optionsBuilder.UseOracle<MeuDbContext>("DATA SOURCE=172.17.0.2:1521/oracle_12g;PERSIST SECURITY INFO=True;USER ID=sys; password=123456; Pooling =False;");
+
+            return new MeuDbContext(optionsBuilder.Options);
+        }
+    }
+}
